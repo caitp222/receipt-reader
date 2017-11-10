a07367f8b4c6ece2d92cf0233ab481cadf256c5b
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-11-09 21:34:42 -0500

    Add form component with styling

---
+++ components/form.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+import { TextInput,
+  StyleSheet,
+  TouchableHighlight,
+  View,
+  Button,
+  Text,
+  AsyncStorage
+} from 'react-native';
+import styles from '../styles/formStyle'
+
+class Form extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.label}>Vendor</Text>
+        <TextInput style={styles.input} name="vendor" />
+        <Text style={styles.label}>Description</Text>
+        <TextInput name="description" style={styles.input}/>
+        <Text style={styles.label}>Amount</Text>
+        <TextInput name="amount" style={styles.input}/>
+        <TouchableHighlight>
+          <Text style={styles.confirm}>Confirm Expense</Text>
+        </TouchableHighlight>
+      </View>
+        )
+      }
+    }
+
+export default Form;
